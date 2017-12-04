69380ac9dac908b67264e4f16fcd507abf06b48d
tarunraja <Rajatarun1>
2017-12-03 23:31:53 -0500

    hash router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter><Route path="/dashboard/:name" component={App}></Route></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><Route path="/dashboard/:name" component={App}></Route></HashRouter>, document.getElementById('root'));
 registerServiceWorker();
